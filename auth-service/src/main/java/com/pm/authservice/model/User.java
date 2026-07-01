43b2bb9d107fec6b3db927776867d5c9507ee361
kiranpagadala1997 <kiranpagadal17@gmail.com>
2026-07-01 18:38:13 +0530

    added two columns

--- auth-service/src/main/java/com/pm/authservice/model/User.java
+++ auth-service/src/main/java/com/pm/authservice/model/User.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
 import java.util.UUID;
 
 
@@ -26,5 +27,11 @@ public class User {
 
     @Column(nullable = false)
     private String role;
+
+    @Column
+    private Date createdAt;
+
+    @Column
+    private String createdBy;
 
 }
